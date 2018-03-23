f8c3e549ec76e7b9cca3c36b8e2c7be3d96d0f60
Anton Kuznetsov <isnifer@gmail.com>
2018-03-23 17:15:36 +0300

    Add docusaurus

---
+++ docs/siteConfig.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'Logo',
+    image: '/docs/img/docusaurus.svg',
+    infoLink: 'https://gettipsi.com',
+    pinned: true,
+  },
+]
+
+const siteConfig = {
+  title: 'tipsi-stripe',
+  tagline: 'Documentation',
+  url: 'https://tipsi.github.io/tipsi-stripe',
+  baseUrl: '/docs/',
+  projectName: 'test-site',
+  headerLinks: [
+    { doc: 'doc1', label: 'Docs' },
+    { doc: 'doc4', label: 'API' },
+    { page: 'help', label: 'Help' },
+    { blog: true, label: 'Blog' },
+  ],
+  users,
+  /* path to images for header/footer */
+  headerIcon: 'img/docusaurus.svg',
+  footerIcon: 'img/docusaurus.svg',
+  favicon: 'img/favicon.png',
+  /* colors for website */
+  colors: {
+    primaryColor: '#2E8555',
+    secondaryColor: '#205C3B',
+  },
+  // custom fonts for website
+  // fonts: {
+    // myFont: ['Times New Roman', 'Serif'],
+    // myOtherFont: ['-apple-system', 'system-ui'],
+  // },
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  organizationName: 'Tipsi', // or set an env variable ORGANIZATION_NAME
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+  scripts: ['https://buttons.github.io/buttons.js'],
+  // You may provide arbitrary config keys to be used as needed by your template.
+  repoUrl: 'https://github.com/facebook/test-site',
+  /* On page navigation for the current documentation page */
+  // onPageNav: 'separate',
+}
+
+module.exports = siteConfig
